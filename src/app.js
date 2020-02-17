049b67729b7debb748265f7d70229cad869de2c1
JY <johnathanlee72@hotmail.com>
2020-02-17 16:35:06 -0700

    added get endpoints and associated tests

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const app = express();
 
 const morganOption = (NODE_ENV === 'production')? 'tiny' : 'common';
 
+const ArticlesService = require('./articles-service');
+
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
@@ -23,9 +25,42 @@ app.use(function errorHandler(error, req, res, next) {
         response = { error: {message: 'server error'} }
     } else {
         console.log(error);
-        response = { message: error.essage, error}
+        response = { message: error.message, error}
     }
     res.status(500).json(response);
+})
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles.map(article => {
+            let datePublished = new Date(article.date_published);
+            datePublished.setHours(datePublished.getHours() - 6);
+            return ({
+                id: article.id,
+                title: article.title,
+                style: article.style,
+                content: article.content,
+                date_published: datePublished,
+            })
+            }))
+        })
+        .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            if(!article) {
+                return res.status(404).json({
+                    error: { message: `Article doesn't exist` }
+                })
+            }
+            res.json(article)
+        })
+        .catch(next)
 })
 
 module.exports = app;
